da3fe1b9480b30a1087563e0e1799e41a42a02ca
DincEmre <65133590+DincEmre@users.noreply.github.com>
2021-08-18 11:49:57 +0300

    Delete backend directory

--- backend/src/main/java/com/example/backend/paket/service/PaketServis.java
+++
@@ -1,8 +0,0 @@
-package com.example.backend.paket.service;
-
-import com.example.backend.customer.model.Customer;
-
-public interface PaketServis {
-    Customer paketTanimla(Customer customer);
-    void paketIptal(Customer customer);
-}
